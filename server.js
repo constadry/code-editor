c3c2454cb38a9bf90b4070244381c4c37f175504
constadry <behappydtworry@gmail.com>
2024-12-15 09:10:39 +0300

    fix: import requir

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const express = require('express');
+import jsonServer from 'json-server';
 
-const app = express();
-const apiServer = jsonServer.create();
+const server = jsonServer.create();
+const router = jsonServer.router('data/db.json');
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router(path.join(__dirname, 'data/db.json'));
 
-// JSON Server по пути /api
-app.use('/api', middlewares, router);
+server.use(middlewares);
+server.use(router);
 
-// Статические файлы Vite
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
